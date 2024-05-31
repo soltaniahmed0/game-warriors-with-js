524353deb2aae2fa1197ce8f75de17618261c9f2
soltaniahmed0 <ahmed0soltani@gmail.com>
2024-05-31 08:45:47 +0100

    11commit

--- fight.js
+++ fight.js
@@ -7,9 +7,8 @@ function startFight(meetingPositions) {
             
             
             for (const blueWarrior of blueWarriors) {
-                // Find the first living red warrior
                 const targetRedWarrior = redWarriors.find(redWarrior => redWarrior.healthPoints > 0);
-                // If there is a living red warrior, let blue warrior attack
+                
                 if (targetRedWarrior) {
                     fight(blueWarrior, targetRedWarrior);
                 }
@@ -23,9 +22,7 @@ function startFight(meetingPositions) {
                     console.log(points);
                     const index = redWarriors.findIndex(warrior => warrior.id === redWarrior.id);
                     if (index !== -1) {
-                        // Remove the defeated warrior from the redWarriors array by ID
                         redWarriors.splice(index, 1);
-                        // Also remove from the redCastle's warriors array
                         redCastle.warriors = redCastle.warriors.filter(warrior => warrior.id !== redWarrior.id);
                     }
                     console.log(redWarriors.length);
@@ -39,12 +36,12 @@ function startFight(meetingPositions) {
             }
             
             console.log("Red attack now ");
-            //alert("redataque");
-            // Red warriors attack blue warriors
+            
+            
             for (const redWarrior of redWarriors) {
-                // Find the first living blue warrior
+                
                 const targetBlueWarrior = blueWarriors.find(blueWarrior => blueWarrior.healthPoints > 0);
-                // If there is a living blue warrior, let red warrior attack
+                
                 if (targetBlueWarrior) {
                     fight(redWarrior, targetBlueWarrior);
                 }
@@ -56,28 +53,29 @@ function startFight(meetingPositions) {
                     console.log(blueWarrior.healthPoints);
                     const index = blueWarriors.findIndex(warrior => warrior.id === blueWarrior.id);
                     if (index !== -1) {
-                        // Remove the defeated warrior from the blueWarriors array by ID
                         blueWarriors.splice(index, 1);
-                        // Also remove from the blueCastle's warriors array
-                        blueCastle.warriors = blueCastle.warriors.filter(warrior => warrior.id !== blueWarrior.id);
+                         blueCastle.warriors = blueCastle.warriors.filter(warrior => warrior.id !== blueWarrior.id);
                     }
+                    if (blueWarriors.length!==0){
+                        console.log('pp'+blueWarriors[0].healthPoints);
+                        blueWarriors[0].healthPoints+=points
+                        console.log(blueWarriors[0].healthPoints);
+                        displayWarriorsOnTrack;
+                      }
                 }
             }
   
-            console.log("Blue warriors:", blueWarriors);
-            console.log("Red warriors:", redWarriors);
-        //}
-        //alert(blueCastle.warriors.length+""+blueWarriors.length+"end redataque"+redCastle.warriors.length+''+redWarriors.length);
-        // Check if blue team is defeated
+            
+        
         if (blueWarriors.length === 0) {
             //alert('Red team wins!');
-            break; // End the fight
+            break; 
         }
   
-        // Check if red team is defeated
+        
         if (redWarriors.length === 0) {
             //alert('Blue team wins!');
-            break; // End the fight
+            break; 
         }
     }
   
@@ -86,38 +84,23 @@ function startFight(meetingPositions) {
     updateResources();
   }
   
-  // Define the sleep function
-  function sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-  }
+
   
   
   
   function fight(attacker, defender) {
-    // Store the list of warriors before the battle
+    
     const initialBlueWarriors = [...blueCastle.warriors];
     const initialRedWarriors = [...redCastle.warriors];
-  
-    // Calculate damage for the attacker
     const damage = attacker.calculateDamage();
-  
-    // Inflict damage on the defender
     defender.healthPoints -= damage;
-    
-    // Check if the defender is defeated
     if (defender.healthPoints <= 0) {
       
       console.log(`${defender.constructor.name} with ID ${defender.id} has been defeated!`);
-      
-      // Remove the defeated warrior from their castle's warriors array
-      const castle = defender.color === 'blue' ? blueCastle : redCastle;
-      castle.warriors = castle.warriors.filter(warrior => warrior.id !== defender.id);
-  
-      // Log the list of warriors before and after the battle
-      console.log(`List of blue warriors before battle:`, initialBlueWarriors);
-      console.log(`List of red warriors before battle:`, initialRedWarriors);
-      console.log(`List of blue warriors after battle:`, blueCastle.warriors);
-      console.log(`List of red warriors after battle:`, redCastle.warriors);
+
+      //const castle = defender.color === 'blue' ? blueCastle : redCastle;
+      //castle.warriors = castle.warriors.filter(warrior => warrior.id !== defender.id);
+ 
     }
   }
   
